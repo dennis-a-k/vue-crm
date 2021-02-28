79440423b6bfb610875e98341844e540488f42f9
dennis-a-k <kargopolov.d.a@gmail.com>
2021-02-28 20:58:36 +0300

    Создание категорий

---
+++ src/store/category.js
@@ -0,0 +1,16 @@
+import firebace from 'firebase/app'
+
+export default {
+  actions: {
+    async createCategory ({ commit, dispatch }, { title, limit }) {
+      try {
+        const uid = await dispatch('getUid')
+        const category = await firebace.database().ref(`/users/${uid}/categories`).push({ title, limit })
+        return { title, limit, id: category.key }
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    }
+  }
+}
