5c3ebf8757366950fe3907bf32610f1a884e5b13
dennis-a-k <kargopolov.d.a@gmail.com>
2021-03-04 19:15:55 +0300

    Действие при не авторизации

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from 'firebase/app'
 
 Vue.use(VueRouter)
 
@@ -7,37 +8,37 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Home.vue')
   },
   {
     path: '/categories',
     name: 'Categories',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Categories.vue')
   },
   {
     path: '/detail-record',
     name: 'DetailRecord',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/DetailRecord.vue')
   },
   {
     path: '/history',
     name: 'History',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/History.vue')
   },
   {
     path: '/planning',
     name: 'Planning',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Planning.vue')
   },
   {
     path: '/profile',
     name: 'Profile',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Profile.vue')
   },
   {
@@ -64,6 +65,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.auth)
+  if (requireAuth && !currentUser) {
+    next('/login?message=login ')
+  } else {
+    next()
+  }
 })
 
 export default router
